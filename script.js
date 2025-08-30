cd9b54ea0d4eb5bc2b8b5271941298dff3192196
CryptoGuardAI2025 <millymaylin2015@icloud.com>
2025-08-30 22:30:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYmoUq4KyZjb5VAU4IYNOLnd8MF7mP13E",
+  authDomain: "dont-push-b6170.firebaseapp.com",
+  projectId: "dont-push-b6170",
+  storageBucket: "dont-push-b6170.appspot.com",
+  messagingSenderId: "813001059051",
+  appId: "1:813001059051:web:5558a33d10fe3c5a4e6f3f",
+  measurementId: "G-7F5MDTT4K6"
+};
+
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
