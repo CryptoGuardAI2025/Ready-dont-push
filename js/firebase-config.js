bc29d24804843cd1971f288cdca901c31dc6546b
CryptoGuardAI2025 <millymaylin2015@icloud.com>
2025-08-31 13:52:00 +0200

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,15 @@
 
-// TODO: Replace with real config
+// Firebase-Konfiguration (deine Daten hier einfügen)
 const firebaseConfig = {
   apiKey: "DEINE_API_KEY",
-  authDomain: "DEIN_AUTH_DOMAIN",
-  databaseURL: "DEINE_DB_URL",
+  authDomain: "DEIN_PROJECT.firebaseapp.com",
+  databaseURL: "https://DEIN_PROJECT.firebaseio.com",
   projectId: "DEIN_PROJECT_ID",
-  storageBucket: "DEIN_BUCKET",
+  storageBucket: "DEIN_PROJECT.appspot.com",
   messagingSenderId: "DEINE_SENDER_ID",
   appId: "DEINE_APP_ID"
 };
+
+// Firebase initialisieren
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
